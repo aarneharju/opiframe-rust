f328dc45bda180c235d3c5a9c4143ac538db8e24
aarneharju <35223549+aarneharju@users.noreply.github.com>
2023-03-16 11:02:22 +0200

    Some Cargo craziness

---
+++ b_hello_world/target/debug/deps/b_hello_world-2416b4afc145964f.d
@@ -0,0 +1,5 @@
+/Users/aarne/Programming/Rust/opiframe-rust/b_hello_world/target/debug/deps/b_hello_world-2416b4afc145964f.rmeta: src/main.rs
+
+/Users/aarne/Programming/Rust/opiframe-rust/b_hello_world/target/debug/deps/b_hello_world-2416b4afc145964f.d: src/main.rs
+
+src/main.rs:
---
+++ b_hello_world/target/debug/deps/b_hello_world-ca98f093c4e17558.d
@@ -0,0 +1,5 @@
+/Users/aarne/Programming/Rust/opiframe-rust/b_hello_world/target/debug/deps/b_hello_world-ca98f093c4e17558.rmeta: src/main.rs
+
+/Users/aarne/Programming/Rust/opiframe-rust/b_hello_world/target/debug/deps/b_hello_world-ca98f093c4e17558.d: src/main.rs
+
+src/main.rs:
