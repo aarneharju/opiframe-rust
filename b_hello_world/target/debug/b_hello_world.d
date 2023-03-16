de00561a6ae07996a291ca90fc84f53c01a8cde0
aarneharju <35223549+aarneharju@users.noreply.github.com>
2023-03-16 10:04:04 +0200

    Added b_hello_world

---
+++ b_hello_world/target/debug/b_hello_world.d
@@ -0,0 +1,1 @@
+/Users/aarne/Programming/Rust/opiframe-rust/b_hello_world/target/debug/b_hello_world: /Users/aarne/Programming/Rust/opiframe-rust/b_hello_world/src/main.rs
---
+++ b_hello_world/target/debug/deps/b_hello_world-4489ad62d73a57b7.d
@@ -0,0 +1,5 @@
+/Users/aarne/Programming/Rust/opiframe-rust/b_hello_world/target/debug/deps/b_hello_world-4489ad62d73a57b7: src/main.rs
+
+/Users/aarne/Programming/Rust/opiframe-rust/b_hello_world/target/debug/deps/b_hello_world-4489ad62d73a57b7.d: src/main.rs
+
+src/main.rs:
